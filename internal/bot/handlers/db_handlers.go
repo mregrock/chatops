97fa9a26c83b0f84c283c166e91b16df43e8fbc4
Denis <dvavramenko@ispras.ru>
2025-06-07 21:14:14 +0300

    Fixed handlers

--- internal/bot/handlers/db_handlers.go
+++ internal/bot/handlers/db_handlers.go
@@ -1,17 +1,32 @@
 package handlers
 
 import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"chatops/internal/monitoring"
+
 	telebot "gopkg.in/telebot.v3"
 )
 
+type Handler struct {
+	monitor *monitoring.Client
+}
+
+func New(monitor *monitoring.Client) *Handler {
+	return &Handler{monitor: monitor}
+}
+
 // db
-func historyHandler(c telebot.Context) error {
+func (h *Handler) historyHandler(c telebot.Context) error {
 	// TODO: Реализовать логику для команды history
 	return c.Send("Выполняется команда history...")
 }
 
 // db
-func operationsHandler(c telebot.Context) error {
+func (h *Handler) operationsHandler(c telebot.Context) error {
 	// TODO: Реализовать логику для команды operations
 	return c.Send("Выполняется команда operations...")
 }
