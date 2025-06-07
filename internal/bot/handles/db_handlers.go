e21a0a9531a45584dd2e32cb064a536f64d0caf7
Denis <dvavramenko@ispras.ru>
2025-06-07 18:58:34 +0300

    Separate by file handlers and add metrics

---
+++ internal/bot/handles/db_handlers.go
@@ -0,0 +1,27 @@
+package db_handler
+
+
+import (
+	"context"
+	"log"
+	"os"
+	"strings"
+	"time"
+	"chatops/internal/monitoring/client.go"
+	"github.com/joho/godotenv"
+	telebot "gopkg.in/telebot.v3"
+)
+
+
+//db
+func historyHandler(c telebot.Context) error {
+	// TODO: Реализовать логику для команды history
+	return c.Send("Выполняется команда history...")
+}
+
+
+//db
+func operationsHandler(c telebot.Context) error {
+	// TODO: Реализовать логику для команды operations
+	return c.Send("Выполняется команда operations...")
+}
