3f9fe30719c128534d919a1c63bbeca5776ed670
toxabrat <toxabrat97@mail.ru>
2025-06-07 17:39:15 +0300

    final add db

--- db/models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct {
-	ID        uint   `gorm:"primaryKey"`
-	Login     string `gorm:"not null"`
-	Password  string `gorm:"not null"`
-	IsDuty    bool   `gorm:"default:false"`
-	JobStatus string `gorm:"not null"`
-}
--- db/models/user_label.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type UserLabel struct {
-	ID     uint   `gorm:"primaryKey"`
-	UserID uint   `gorm:"not null"`
-	User   User   `gorm:"foreignKey:UserID"`
-	Label  string `gorm:"not null"`
-}
