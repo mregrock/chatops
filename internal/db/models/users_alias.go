c4159646e6b670349a89566a588bdf984f2524ff
toxabrat <toxabrat97@mail.ru>
2025-06-07 18:26:30 +0300

    delete unused file

--- internal/db/models/users_alias.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type UserAlias struct {
-	ID     uint   `gorm:"primaryKey"`
-	UserID uint   `gorm:"not null"`
-	User   User   `gorm:"foreignKey:UserID"`
-	Alias  string `gorm:"not null"`
-}
