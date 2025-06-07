f9b6be21958f3f64a2e66d5c27b2df1a2a189d07
mregrock <mregrock@yandex.ru>
2025-06-07 19:59:34 +0300

    Small fixes

--- internal/bot/handlers/db_handlers.go
+++ internal/bot/handlers/db_handlers.go
@@ -1,26 +1,16 @@
 package handlers
 
-
 import (
-	"context"
-	"log"
-	"os"
-	"strings"
-	"time"
-	"chatops/internal/monitoring/client.go"
-	"github.com/joho/godotenv"
 	telebot "gopkg.in/telebot.v3"
 )
 
-
-//db
+// db
 func historyHandler(c telebot.Context) error {
 	// TODO: Реализовать логику для команды history
 	return c.Send("Выполняется команда history...")
 }
 
-
-//db
+// db
 func operationsHandler(c telebot.Context) error {
 	// TODO: Реализовать логику для команды operations
 	return c.Send("Выполняется команда operations...")
--- internal/bot/handlers/metrics_handlers.go
+++ internal/bot/handlers/metrics_handlers.go
@@ -1,22 +1,26 @@
 package handlers
 
-
 import (
 	"context"
-	"log"
-	"os"
+	"fmt"
 	"strings"
 	"time"
-	"chatops/internal/monitoring/handlers"
-	"github.com/joho/godotenv"
+
+	"hackaton/internal/monitoring"
+
 	telebot "gopkg.in/telebot.v3"
 )
 
+type Handler struct {
+	monitor *monitoring.Client
+}
 
+func New(monitor *monitoring.Client) *Handler {
+	return &Handler{monitor: monitor}
+}
 
-
-//metric
-func metricHandler(c telebot.Context) error {
+// metric
+func (h *Handler) MetricHandler(c telebot.Context) error {
 	parts := strings.SplitN(c.Text(), " ", 3)
 	if len(parts) < 3 {
 		return c.Send("Неправильное кол-во параметров ")
@@ -24,45 +28,41 @@ func metricHandler(c telebot.Context) error {
 	service := parts[1]
 	metric := parts[2]
 	req := metric + "{job=\"" + service + "\"}"
-	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	response, err := client.Query(ctx, req)
+	response, err := h.monitor.Query(ctx, req)
 	if err != nil {
 		if ctx.Err() == context.DeadlineExceeded {
 			return c.Send("Превышено время ожидания запроса (timeout)")
 		}
 		return c.Send(fmt.Sprintf("Произошла ошибка: %v", err))
 	}
-	
+
 	result := fmt.Sprintf("Status: %s\n", response.Status)
 
-	// TODO: че блять 
+	// TODO: че блять
 	for i, res := range response.Data.Result {
-    	result += fmt.Sprintf("data.result[%d].value: %v\n", i, res.Value)
+		result += fmt.Sprintf("data.result[%d].value: %v\n", i, res.Value)
 	}
 
 	return c.Send(result)
 
 }
 
-
-
-
-//metric
-func listMetricsHandler(c telebot.Context) error {
+// metric
+func (h *Handler) ListMetricsHandler(c telebot.Context) error {
 	parts := strings.SplitN(c.Text(), " ", 3)
 	if len(parts) < 3 {
 		return c.Send("Неправильное кол-во параметров ")
 	}
-	client = NewClient("", "")
 	service := parts[1]
 	req := service
 	metric := parts[2]
-	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	response, err := client.ListMetrics(ctx, req)
+	response, err := h.monitor.ListMetrics(ctx, req)
 	if err != nil {
 		if ctx.Err() == context.DeadlineExceeded {
 			return c.Send("Превышено время ожидания запроса (timeout)")
@@ -79,4 +79,32 @@ func listMetricsHandler(c telebot.Context) error {
 	return c.Send(strings.Join(matchedMetrics, "\n"))
 }
 
+// StatusHandler - заглушка
+func (h *Handler) StatusHandler(c telebot.Context) error {
+	return c.Send("Not implemented")
+}
 
+// ScaleHandler - заглушка
+func (h *Handler) ScaleHandler(c telebot.Context) error {
+	return c.Send("Not implemented")
+}
+
+// RestartHandler - заглушка
+func (h *Handler) RestartHandler(c telebot.Context) error {
+	return c.Send("Not implemented")
+}
+
+// RollbackHandler - заглушка
+func (h *Handler) RollbackHandler(c telebot.Context) error {
+	return c.Send("Not implemented")
+}
+
+// HistoryHandler - заглушка
+func (h *Handler) HistoryHandler(c telebot.Context) error {
+	return c.Send("Not implemented")
+}
+
+// OperationsHandler - заглушка
+func (h *Handler) OperationsHandler(c telebot.Context) error {
+	return c.Send("Not implemented")
+}
